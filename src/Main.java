2caec15b69320b509478cc4dfc813ee44a18b3cb
Vladimir Muraito <vl.muraito@mail.ru>
2024-06-03 04:29:35 +1000

    Прошу проверить ДЗ.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,64 @@
+import model.GryffindorStudent;
+import model.HufflepuffStudent;
+import model.RavenclawStudent;
+import model.SlytherinStudent;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("\nАкадемия Чародейства и Волшебства Хогвартс.\n");
+
+        GryffindorStudent harryPotter = new GryffindorStudent("Гарри Потер", 10, 12,
+                6, 7, 10);
+        GryffindorStudent hermioneGranger = new GryffindorStudent("Гермиона Грейнджер", 8,
+                10, 5, 5, 6);
+        GryffindorStudent ronWeasley = new GryffindorStudent("Рон Уизли", 6, 8,
+                3, 6, 5);
+
+        HufflepuffStudent zachariahSmith = new HufflepuffStudent("Захария Смит", 4,
+                4, 3, 2, 1);
+        HufflepuffStudent cedricDiggory = new HufflepuffStudent("Седрик Диггори", 7,
+                6, 5, 4, 5);
+        HufflepuffStudent justinFinchFletchley = new HufflepuffStudent("Джастин Финч-Флетчли", 2,
+                6, 4, 2, 2);
+
+        RavenclawStudent zhouChang = new RavenclawStudent("Чжоу Чанг", 5, 2,
+                1, 8, 6, 4);
+        RavenclawStudent padmaPatil = new RavenclawStudent("Падма Патил", 3, 5,
+                6, 3, 1, 3);
+        RavenclawStudent marcusBelby = new RavenclawStudent("Маркус Белби", 9, 2,
+                5, 5, 8, 2);
+
+        SlytherinStudent dracoMalfoy = new SlytherinStudent("Драко Малфой", 7, 5,
+                7, 6, 8, 4, 8);
+        SlytherinStudent grahamMontague = new SlytherinStudent("Грэхэм Монтегю", 4,
+                6, 4, 2, 5, 3, 4);
+        SlytherinStudent gregoryGoyle = new SlytherinStudent("Грегори Гойл", 3, 6,
+                2, 5, 3, 1, 4);
+
+
+        System.out.println(harryPotter + "\n" + hermioneGranger + "\n" +
+                ronWeasley + "\n" + zachariahSmith + "\n" +
+                cedricDiggory + "\n" +
+                justinFinchFletchley + "\n" +
+                zhouChang + "\n" +
+                padmaPatil + "\n" +
+                marcusBelby + "\n" +
+                dracoMalfoy + "\n" +
+                grahamMontague + "\n" +
+                gregoryGoyle + "\n");
+
+
+        hermioneGranger.printGryffindorDifferenceProperties(harryPotter);
+        zachariahSmith.printHufflepuffDifferenceProperties(justinFinchFletchley);
+        marcusBelby.printRavenclawDifferenceProperties(zhouChang);
+        dracoMalfoy.printSlytherinDifferenceProperties(gregoryGoyle);
+
+        System.out.println();
+        zhouChang.printPowerDifference(harryPotter);
+        dracoMalfoy.printPowerDifference(hermioneGranger);
+        cedricDiggory.printPowerDifference(grahamMontague);
+        ronWeasley.printPowerDifference(marcusBelby);
+        padmaPatil.printPowerDifference(justinFinchFletchley);
+
     }
 }
--- src/model/GryffindorStudent.java
+++ src/model/GryffindorStudent.java
@@ -1,4 +1,44 @@
 package model;
 
-public class GryffindorStudent {
+public class GryffindorStudent extends HogwartsStudent {
+    private int nobility;
+    private int honor;
+    private int bravery;
+
+    public GryffindorStudent(String name,
+                             int magicalPower,
+                             int transgressionDistance,
+                             int nobility,
+                             int honor,
+                             int bravery) {
+        super(name, magicalPower, transgressionDistance);
+        this.nobility = nobility;
+        this.honor = honor;
+        this.bravery = bravery;
+    }
+
+    public void printGryffindorDifferenceProperties(GryffindorStudent student) {
+        if (this.getSumProperties() > student.getSumProperties()) {
+            System.out.printf("%s лучше как Гриффиндорец, чем %s%n", this.getName(), student.getName());
+        } else if (student.getSumProperties() > this.getSumProperties()) {
+            System.out.printf("%s хуже как Гриффиндорец, чем %s%n", this.getName(), student.getName());
+        } else {
+            System.out.printf("%s и %s хороши как Гриффиндорцы в равной степени", this.getName(), student.getName());
+        }
+    }
+
+    protected int getSumProperties() {
+        return this.nobility + this.honor + this.bravery;
+    }
+
+    @Override
+    public String toString() {
+        return "Студент Гриффиндора " +
+                "(Имя: " + getName() +
+                "; Магическая мощь: " + getMagicalPower() +
+                "; Дальность трансгрессии: " + getTransgressionDistance() +
+                "; Благородство: " + nobility +
+                "; Честь: " + honor +
+                "; Храбрость: " + bravery + ')';
+    }
 }
